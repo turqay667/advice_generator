c0197ef6a8cdd09e206d9488ecf4c6888981602f
Turgay Mammadov <memmedovturqay871@gmail.com>
2023-03-25 16:46:21 +0400

    uploading

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+const fetchAdvice=()=>{
+    fetch('https://api.adviceslip.com/advice')
+    .then(res=>{
+        return res.json()
+    })
+    .then(advice=>{
+        const adviceIdn=advice.slip.id
+        const adviceText=advice.slip.advice
+        advicetext.innerHTML=adviceText
+        adviceId.innerHTML=adviceIdn
+    })
+    .catch(err=>
+console.log(err))
+}
+const btn=document.getElementById('btn')
+const adviceId=document.getElementById('adviceId')
+const advicetext=document.getElementById('adviceText')
+btn.addEventListener('click',()=>{
+    fetchAdvice()
+})
+
+window.onload=()=>{
+    fetchAdvice()
+}
